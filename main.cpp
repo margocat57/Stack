f3680100b5765c93c9d16ee94942af5f837039f6
margocat57 <bin242717@gmail.com>
2025-09-24 17:11:17 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <stdint.h>
+#include <time.h>
 
 typedef int stack_elem_t;
 
@@ -12,13 +13,19 @@ bool DEBUG = true;
 
 // TODO сделать массив стуктур который принимает масимальные значения каждого из типов и спецификатор формата, возм вставить это в макрос
 
-// TODO добавить ошибки в log file
-#define MISTAKE_INFO(stk_ptr, mistake)                                                                              \
-    if (DEBUG)                                                                                                      \
-    {                                                                                                               \
-        fprintf(stderr, "Mistake from file %s function %s and string %d", __FILE__, __PRETTY_FUNCTION__, __LINE__); \
-        StackDump(stk_ptr, mistake);                                                                                \
-        return mistake;                                                                                             \
+#define MISTAKE_INFO(stk_ptr, mistake, str, log_file_ptr)                                                                 \
+    if (DEBUG && (mistake))                                                                                               \
+    {                                                                                                                     \
+        PrintMistakeTime(log_file_ptr);                                                                                   \
+        fprintf(log_file_ptr, "Mistake from file %s function %s and string %d", __FILE__, __PRETTY_FUNCTION__, __LINE__); \
+        StackDump(stk_ptr, mistake, log_file_ptr);                                                                        \
+        fprintf(log_file_ptr, "\nData was printed in reason verify failed " #str " using func\n");                        \
+        fprintf(log_file_ptr, "\n");                                                                                      \
+        if (log_file_ptr != NULL)                                                                                         \
+        {                                                                                                                 \
+            fflush(log_file_ptr);                                                                                         \
+        }                                                                                                                 \
+        return mistake;                                                                                                   \
     }
 
 struct stack_t_t
@@ -35,38 +42,83 @@ enum StackErr_t
     DATA_NULL_PTR = 2,
     OUT_OF_INDEX = 3,
     INCORR_SIZE = 4,
-    VAR_OVERFLOW = 5 // для элементов массива
+    VAR_OVERFLOW = 5,
+    VAR_NULL_PTR = 6,
+    LOG_FILE_OPEN_ERROR = 7
 };
 
 stack_elem_t POISON = -6666;
 
-StackErr_t StackErr(stack_t_t *stk)
+// FILE *log_file_ptr = NULL;
+
+FILE *GetLogStream(FILE *log_file_ptr)
 {
+    if (log_file_ptr == NULL)
+    {
+        fprintf(stderr, "NULL .log file pointer. Info will be outputted to stderr\n");
+        return stderr;
+    }
+    return log_file_ptr;
+}
+
+FILE *OpenLogFile(const char *file_name)
+{
+    if (file_name == NULL)
+    {
+        fprintf(stderr, "NULL pointer to file name. Function will return pointer to stderr");
+        return stderr;
+    }
+    FILE *log_file_ptr = fopen(file_name, "a+");
+    log_file_ptr = GetLogStream(log_file_ptr);
+
+    return log_file_ptr;
+}
+
+void PrintMistakeTime(FILE *log_file_ptr)
+{
+    log_file_ptr = GetLogStream(log_file_ptr);
+
+    time_t now = time(NULL);
+    char time_str[20] = {};
+    strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", localtime(&now));
+
+    fprintf(log_file_ptr, "[%s]: ", time_str);
+}
+
+void CloseLogFile(FILE *log_file_ptr)
+{
+    if (!log_file_ptr)
+        return;
+
+    fclose(log_file_ptr);
+}
+
+StackErr_t StackErr(stack_t_t *stk, FILE *log_file_ptr)
+{
+    log_file_ptr = GetLogStream(log_file_ptr);
+
     if (stk == NULL)
     {
-        fprintf(stderr, "Accessing to NULL pointer\n");
+        fprintf(log_file_ptr, "Accessing to NULL pointer\n");
         return STK_NULL_PTR;
     }
     else if (stk->ptr > stk->capacity)
     {
-        fprintf(stderr, "Trying to get access to the elem out of array\n");
+        fprintf(log_file_ptr, "Trying to get access to the elem out of array\n");
         return OUT_OF_INDEX;
     }
     return NO_MISTAKE;
 }
 
-void StackDump(stack_t_t *stk, StackErr_t err)
+void StackDump(stack_t_t *stk, StackErr_t err, FILE *log_file_ptr)
 {
-    if (err == NO_MISTAKE)
-    {
-        return;
-    }
+    log_file_ptr = GetLogStream(log_file_ptr);
 
-    fprintf(stderr, "\n");
-    fprintf(stderr, "stack [%p] ", stk);
+    fprintf(log_file_ptr, "\n");
+    fprintf(log_file_ptr, "stack [%p] ", stk);
     if (err == STK_NULL_PTR)
     {
-        fprintf(stderr, "BAD!!!");
+        fprintf(log_file_ptr, "BAD!!!");
         return;
     }
 
@@ -74,18 +126,18 @@ void StackDump(stack_t_t *stk, StackErr_t err)
     {
         return;
     }
-    fprintf(stderr, "\ncapacity = %lu ", stk->capacity);
+    fprintf(log_file_ptr, "\ncapacity = %lu ", stk->capacity);
 
-    fprintf(stderr, "\nptr = %lu ", stk->ptr);
+    fprintf(log_file_ptr, "\nptr = %lu ", stk->ptr);
     if (err == OUT_OF_INDEX)
     {
-        fprintf(stderr, "BAD!!!");
+        fprintf(log_file_ptr, "BAD!!!");
     }
 
-    fprintf(stderr, "\ndata [%p] ", stk->data);
+    fprintf(log_file_ptr, "\ndata [%p] ", stk->data);
     if (err == DATA_NULL_PTR)
     {
-        fprintf(stderr, "BAD!!!");
+        fprintf(log_file_ptr, "BAD!!!");
         return;
     }
 
@@ -94,44 +146,39 @@ void StackDump(stack_t_t *stk, StackErr_t err)
         // TODO как правильно работать с типами данных при печати если мы точно не знаем тип
         if (stk->data[idx] != POISON)
         {
-            fprintf(stderr, "\n^[%lu] = %d", idx, stk->data[idx]);
+            fprintf(log_file_ptr, "\n^[%lu] = %d", idx, stk->data[idx]);
         }
         else
         {
-            fprintf(stderr, "\n[%lu] = %d (POISON)", idx, stk->data[idx]);
+            fprintf(log_file_ptr, "\n[%lu] = %d (POISON)", idx, stk->data[idx]);
         }
     }
 }
 
-StackErr_t StackCtor(stack_t_t *stk, long long int size)
+StackErr_t StackCtor(stack_t_t *stk, long long int size, FILE *log_file_ptr)
 {
-    StackErr_t mistake = StackErr(stk);
+    log_file_ptr = GetLogStream(log_file_ptr);
 
-    if (mistake)
-    {
-        MISTAKE_INFO(stk, mistake);
-    }
+    StackErr_t mistake = StackErr(stk, log_file_ptr);
+    MISTAKE_INFO(stk, mistake, before, log_file_ptr);
 
     if (size <= 0 || size > LLONG_MAX)
     {
-        fprintf(stderr, "Try to set incorrect size of data\n");
-        fprintf(stderr, "size = %lld\n", size);
-        MISTAKE_INFO(stk, INCORR_SIZE);
+        fprintf(log_file_ptr, "Try to set incorrect size of data\n");
+        fprintf(log_file_ptr, "size = %lld\n", size);
+        MISTAKE_INFO(stk, INCORR_SIZE, before, log_file_ptr);
     }
 
     stk->capacity = (size_t)size;
     stk->data = (stack_elem_t *)calloc(stk->capacity + SAVE, sizeof(stack_elem_t *));
 
-    mistake = StackErr(stk);
-    if (mistake)
-    {
-        MISTAKE_INFO(stk, mistake);
-    }
+    mistake = StackErr(stk, log_file_ptr);
+    MISTAKE_INFO(stk, mistake, after, log_file_ptr);
 
     if (stk->data == NULL)
     {
-        fprintf(stderr, "Allocation error\n");
-        MISTAKE_INFO(stk, DATA_NULL_PTR);
+        fprintf(log_file_ptr, "Allocation error\n");
+        MISTAKE_INFO(stk, DATA_NULL_PTR, after, log_file_ptr);
     }
 
     for (size_t i = 0; i <= stk->capacity; i++)
@@ -142,18 +189,18 @@ StackErr_t StackCtor(stack_t_t *stk, long long int size)
     return NO_MISTAKE;
 }
 
-StackErr_t StackPush(stack_t_t *stk, long long int value)
+StackErr_t StackPush(stack_t_t *stk, long long int value, FILE *log_file_ptr)
 {
-    StackErr_t mistake = StackErr(stk);
+    log_file_ptr = GetLogStream(log_file_ptr);
 
-    if (mistake)
-        MISTAKE_INFO(stk, mistake);
+    StackErr_t mistake = StackErr(stk, log_file_ptr);
+    MISTAKE_INFO(stk, mistake, before, log_file_ptr);
 
     // FIXME работать с разными типами мы сфокусированы на int
     if (value > INT_MAX)
     {
-        fprintf(stderr, "Varirable overflow\n");
-        MISTAKE_INFO(stk, VAR_OVERFLOW);
+        fprintf(log_file_ptr, "Varirable overflow\n");
+        MISTAKE_INFO(stk, VAR_OVERFLOW, before, log_file_ptr);
     }
 
     if (stk->ptr == 0)
@@ -164,42 +211,44 @@ StackErr_t StackPush(stack_t_t *stk, long long int value)
     if (stk->ptr != stk->capacity - 1)
         stk->ptr++;
 
-    mistake = StackErr(stk);
-
-    if (mistake)
-        MISTAKE_INFO(stk, mistake);
+    mistake = StackErr(stk, log_file_ptr);
+    MISTAKE_INFO(stk, mistake, after, log_file_ptr);
 
     return NO_MISTAKE;
 }
 
-stack_elem_t StackPop(stack_t_t *stk)
+stack_elem_t StackPop(stack_t_t *stk, stack_elem_t *elem, FILE *log_file_ptr)
 {
-    // Вот здесь логика реализации немного смущает
-    StackErr_t mistake = StackErr(stk);
+    log_file_ptr = GetLogStream(log_file_ptr);
 
-    if (mistake)
-        MISTAKE_INFO(stk, mistake);
+    // Вот здесь логика реализации немного смущает
+    StackErr_t mistake = StackErr(stk, log_file_ptr);
+    MISTAKE_INFO(stk, mistake, before, log_file_ptr);
+
+    if (elem == NULL)
+    {
+        fprintf(log_file_ptr, "NULL POINTER to elem where value is written\n");
+        MISTAKE_INFO(stk, VAR_NULL_PTR, before, log_file_ptr);
+    }
 
     if (stk->ptr != 1)
         stk->ptr--;
 
-    stack_elem_t elem = stk->data[stk->ptr];
+    *elem = stk->data[stk->ptr];
     stk->data[stk->ptr] = POISON;
 
-    mistake = StackErr(stk);
+    mistake = StackErr(stk, log_file_ptr);
+    MISTAKE_INFO(stk, mistake, after, log_file_ptr);
 
-    if (mistake)
-        MISTAKE_INFO(stk, mistake);
-
-    return elem;
+    return *elem;
 }
 
-stack_elem_t StackTop(stack_t_t *stk)
+stack_elem_t StackTop(stack_t_t *stk, FILE *log_file_ptr)
 {
-    StackErr_t mistake = StackErr(stk);
+    log_file_ptr = GetLogStream(log_file_ptr);
 
-    if (mistake)
-        MISTAKE_INFO(stk, mistake);
+    StackErr_t mistake = StackErr(stk, log_file_ptr);
+    MISTAKE_INFO(stk, mistake, before, log_file_ptr);
 
     return stk->data[stk->ptr];
 }
@@ -208,24 +257,23 @@ int main()
 {
     stack_t_t stk1 = {};
 
+    FILE *log_file1 = OpenLogFile("mistakes.log");
+
+    // FILE *log_file2 = OpenLogFile(NULL);
+
     printf("\n1test_start\n");
-    StackErr_t err1 = StackCtor(&stk1, -5);
-    if (err1)
-        StackDump(&stk1, err1);
+    StackErr_t err1 = StackCtor(&stk1, -5, log_file1);
     printf("\n1st TESTING SIZE\n");
 
     printf("\n2test_start\n");
-    StackErr_t err2 = StackCtor(NULL, 5);
-    if (err2)
-        StackDump(NULL, err2);
+    StackErr_t err2 = StackCtor(NULL, 5, log_file1);
     printf("\n2nd TESTING NULL_PTR\n");
 
-    StackCtor(&stk1, 5);
-
-    StackPush(&stk1, 1);
-    StackPush(&stk1, 2);
-    StackPush(&stk1, 3);
-    StackPush(&stk1, 4);
+    StackCtor(&stk1, 5, log_file1);
+    StackPush(&stk1, 1, log_file1);
+    StackPush(&stk1, 2, NULL);
+    StackPush(&stk1, 3, log_file1);
+    StackPush(&stk1, 4, log_file1);
     /*
     printf("\n3test_start\n");
     StackErr_t err3 = StackPush(&stk1, 5);
@@ -235,11 +283,12 @@ int main()
     */
 
     printf("\n4test_start\n");
-    StackPop(&stk1);
-    StackErr_t err4 = StackPush(&stk1, 100000000000000);
-    if (err4)
-        StackDump(&stk1, err4);
+    stack_elem_t x = 0;
+    StackPop(&stk1, NULL, log_file1);
+    StackErr_t err4 = StackPush(&stk1, 100000000000000, log_file1);
     printf("\n4th TESTING_BIG_DIGIT\n");
+
+    CloseLogFile(log_file1);
 
     // int x = 100000000000;
     // printf("%d", x);
